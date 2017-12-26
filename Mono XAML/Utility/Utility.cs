caa25718271a9ee0861bb00e357bc98baad7c6f1
Daniel Everland <daniel@everlandgames.com>
2017-12-26 01:24:47 +0100

    Added clamp functions

--- Mono XAML/Utility/Utility.cs
+++ Mono XAML/Utility/Utility.cs
@@ -7,6 +7,26 @@ namespace MonoXAML
 {
     public static class Utility
     {
+        public static int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+                return min;
+
+            if (value > max)
+                return max;
+
+            return value;
+        }
+        public static float Clamp(float value, float min, float max)
+        {
+            if (value < min)
+                return min;
+
+            if (value > max)
+                return max;
+
+            return value;
+        }
         public static bool IsSolidColor(Brush brush)
         {
             return brush is SolidColorBrush;
