570e202738e51912550a11683b8742fb47d554fb
Daniel Everland <daniel@everlandgames.com>
2017-12-26 01:25:55 +0100

    Removed stroke color property in rectangle class

--- Mono XAML/Objects/Renderable Elements/RenderableRectangle.cs
+++ Mono XAML/Objects/Renderable Elements/RenderableRectangle.cs
@@ -14,7 +14,6 @@ namespace MonoXAML.Objects
 
         public Texture2D FillTexture { get { return _fillTexture; } set { _fillTexture = value; } }
 
-        public Microsoft.Xna.Framework.Color StrokeColor { get { return Utility.GetColor(_rectangle.Stroke); } }
         public Microsoft.Xna.Framework.Color FillColor { get { return Utility.GetColor(_rectangle.Fill); } }
 
         private System.Windows.Shapes.Rectangle _rectangle { get { return (System.Windows.Shapes.Rectangle)Element; } }
